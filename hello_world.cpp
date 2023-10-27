ab945db3970c9d1bfd85245d2bd65f881dcf3f2e
UTK@RSH_thedecoder <117360094+utkarshjain123@users.noreply.github.com>
2023-10-28 01:42:01 +0530

    Create hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,18 @@
+/*
+PRN - 22070123127
+Exp_1 
+Part 1*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"HELLO WORLD"<<endl;
+    cout<<"My first program"<<endl;
+    
+
+/*
+OUTPUT
+HELLO WORLD
+My first program
+*/
